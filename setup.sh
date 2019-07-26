67102d3f98f9741529d889d5583d15ef12ce2de8
RealDeanZhao <zestxjest@outlook.com>
2019-07-26 13:30:06 +0800

    Remove the container

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 source ./common.sh
 
 function main() {
-    flagmain=true
     echo -e $YELLOW'Please select an option: \n' \
         $GREEN'1) Create Basic Node \n' \
         $PINK'2) Create Genesis Batch \n' \
@@ -15,19 +14,18 @@ function main() {
 
     case $option in
     1)
-        docker run -it -v ${PWD}:/sawtooth-network-setup zestxjest/sawtooth-network-setup:latest ./node.sh $@
+        docker run -it --name sawtooth-network-setup -v ${PWD}:/sawtooth-network-setup zestxjest/sawtooth-network-setup:latest ./node.sh $@
         ;;
     2)
-        docker run -it -v ${PWD}:/sawtooth-network-setup zestxjest/sawtooth-network-setup:latest ./genesis.sh $@
+        docker run -it --name sawtooth-network-setup -v ${PWD}:/sawtooth-network-setup zestxjest/sawtooth-network-setup:latest ./genesis.sh $@
         ;;
     3)
         ./node-start.sh $@
         ;;
-    3)
-        flagmain=false
-        ;;
-    esac
+    3) ;;
 
+    esac
+    docker rm -f sawtooth-network-setup
 }
 
 main $@
